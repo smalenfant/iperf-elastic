ba4118bc2ebdfaad6d391bc2ae316dc99cb29b26
Steve Malenfant <steve.malenfant@cox.com>
2018-03-13 13:14:31 -0400

    Adding parameters and upstream test

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Performance verification using iperf
 
 This is intended to connect to a remote iperf server to test latency/jitter/drop
+2 measurements will be sent, upstream and downstream
 
 ### Variables used
 
 - iperf_host: Target iperf server (default to 127.0.0.1)
 - iperf_port: Target iperf port   (default 5201)
+- params: Set the parameter for iperf (Default, --bandwidth 1m)
 - location: (optional) Location where the test originate from
 - elastic_url: Elasticsearch URL to post data (default to http://elasticsearch:9200)
 - HOSTNAME: Can be used to override the docker generate hostname
 
 - Example:
 
-docker run -it --rm -e host=127.0.0.1 -e port=5201 -e HOSTNAME=me -e location="here" -e elastic_url=http://127.0.0.1:9200 iperf:latest 
+docker run -it --rm -e host=127.0.0.1 -e params="--bandwidth 100k --udp --length 60" -e elastic_url=http://127.0.0.1:9200 iperf:latest 
 
--- run-iperf.py
+++ run-iperf.py
@@ -9,6 +9,7 @@ from elasticsearch import Elasticsearch
 elastic_url = os.getenv('elastic_url', 'http://elasticsearch:9200')
 iperf_host  = os.getenv('iperf_host', '127.0.0.1')
 iperf_port  = os.getenv('iperf_port', '5201')
+params      = os.getenv('params','--bandwidth 1m')
 
 mapping = '''
 {
@@ -25,20 +26,40 @@ mapping = '''
 }
 '''
 
-command = "iperf3 --client " + iperf_host + " --port " + iperf_port + " --json -b 100k -u -l 25 -J -i 0"
-args = shlex.split(command)
-print args
-proc = subprocess.Popen(args,stdout=subprocess.PIPE)
-(out, err) = proc.communicate()
-
-doc = json.loads(out)
-doc['hostname'] = os.environ['HOSTNAME']
-doc['location'] = os.getenv('location', 'unknown')
-
-# Ship data to Elastic Search
+# Connect and set mapping
 es = Elasticsearch(elastic_url,verify_certs=False)
-index = 'latency-' + datetime.fromtimestamp(doc['start']['timestamp']['timesecs']).strftime('%Y.%m.%d')
+index = 'latency-' + datetime.today().strftime('%Y.%m.%d')
 if not es.indices.exists(index):
   es.indices.create(index, ignore=400, body=mapping)
 
-results = es.index(index=index, doc_type="doc", body=doc)
+command = ['iperf3', '--json', '-interval', '0']
+
+# Add Client/Target
+command.append('--client')
+command.append(iperf_host)
+
+# Add port
+command.append('--port')
+command.append(iperf_port)
+
+command.extend(shlex.split(params))
+ 
+def run_iperf (command):
+  print command
+  #args = shlex.split(command)
+  proc = subprocess.Popen(command,stdout=subprocess.PIPE)
+  (out, err) = proc.communicate()
+
+  doc = json.loads(out)
+  doc['hostname'] = os.environ['HOSTNAME']
+  doc['location'] = os.getenv('location', 'unknown')
+  #doc['params'] = command
+
+  es.index(index=index, doc_type="doc", body=doc)
+
+# Run once (downstream)
+run_iperf(command)
+
+# Run again (upstream)
+command.append('--reverse')
+run_iperf(command)
